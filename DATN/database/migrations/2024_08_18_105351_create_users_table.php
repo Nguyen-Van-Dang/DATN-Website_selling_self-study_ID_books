6b328513d1c4f9ca955339e4b288ef6e2f987ae6
dang12e <ctytnhh5thanhvienvogiacu@gmail.com>
2024-08-18 18:12:25 +0700

    [DATN] - init code

---
+++ DATN/database/migrations/2024_08_18_105351_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', length: 255)->nullable();
+            $table->string('email', length: 255)->unique();
+            $table->string('phone', length: 10)->nullable();
+            $table->string('password', length: 255);
+            $table->integer('loginType')->default(1)->nullable();
+            $table->string('token', length: 10)->nullable();
+            $table->foreignId('role_id')->nullable()->constrained()->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};
---
+++ DATN/database/migrations/2024_08_18_110323_create_results_table.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            
+            $table->float('score');
+            $table->integer('number_correct');
+            $table->integer('number_incorrect');
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
+            $table->foreignId('test_id')->nullable()->constrained('tests')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+
+        Schema::create('tests', function (Blueprint $table) {
+            $table->id();
+
+
+            $table->timestamps();
+        });
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+
+
+            $table->timestamps();
+        });
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+
+            $table->timestamps();
+        });
+
+        Schema::create('user_answers', function (Blueprint $table) {
+            $table->id();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_answers');
+        Schema::dropIfExists('results');
+        Schema::dropIfExists('answers');
+        Schema::dropIfExists('questions');
+        Schema::dropIfExists('exams');
+    }
+};
