5882df832b9bd5373115b01639d82ec78086e362
dang12e <ctytnhh5thanhvienvogiacu@gmail.com>
2024-08-19 13:05:11 +0700

    [DATN] - Migrate

---
+++ DATN/database/migrations/2024_08_18_000020_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 255)->nullable();
+            $table->text('description')->nullable();
+            $table->timestamp('payment_date')->nullable();
+            $table->foreignId('order_id')->nullable()->constrained('orders')->onDelete('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
